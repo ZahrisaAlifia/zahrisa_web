0798cebcd269914c5f5c6dd273375368e7e0c7fd
ZahrisaAlifia <za.alifa.ara@gmail.com>
2023-11-02 19:09:11 +0700

    first commit

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -1,35 +1,46 @@
 <?php
-defined('BASEPATH') or exit('no direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class web extends CI_Controller
+class Web extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
+        $this->load->model('m_data');
         $this->load->helper('url');
+    }
+    public function user()
+    {
+        $data['user'] = $this->m_data->ambil_data()->result();
+        $data = array(
+            'user' => $this->m_data->ambil_data()->result(),
+            'judul' => "Daftar User"
+
+        );
+        $this->load->view('v_header', $data);
+        $this->load->view('v_user', $data);
+        $this->load->view('v_footer', $data);
     }
 
     public function index()
     {
         $data['judul'] = "Home";
         $this->load->view('v_header', $data);
-        $this->load->view('v_index', $data);
-        $this->load->view('v_footer', $data);
-    }
-
-    public function blog()
-    {
-        $data['judul'] = "Blog";
-        $this->load->view('v_header', $data);
         $this->load->view('v_blog', $data);
         $this->load->view('v_footer', $data);
     }
-
     public function about()
     {
-        $data['judul'] = "About";
+        $data['judul'] = "Halaman about";
         $this->load->view('v_header', $data);
         $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+    public function blog()
+    {
+        $data['judul'] = "Halaman blog";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_blog', $data);
         $this->load->view('v_footer', $data);
     }
 }
